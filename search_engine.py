8a2c4632c611554072b5723e4dd6c5e74771d648
oscarko84 <gaoyushuo@gmail.com>
2025-04-17 15:17:30 +0900

    수정 2

--- search_engine.py
+++ search_engine.py
@@ -56,4 +56,11 @@ class SearchEngine:
         #         ]
         #     }
         # }
-        return {"version":"2.0","template":{"outputs":[{"simpleText":{"text":"hello I'm Ryan"}}]}
+        return {
+            "version":"2.0",
+            "template":{
+                "outputs":[
+                    {"simpleText":{"text":"hello I'm Ryan"}}
+                    ]
+                    }
+        }
